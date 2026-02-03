0947c38fdef3239564e00fe650ac040be9f29904
Palak Narang <163756539+palaknarang05@users.noreply.github.com>
2026-02-04 01:30:47 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd backend
-# Install dependencies
-pip install -r requirements.txt
-# Run migrations
-python manage.py migrate
-# Start server
-gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
