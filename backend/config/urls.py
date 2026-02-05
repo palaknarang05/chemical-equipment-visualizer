783d8e649be48413db6a122963e5d2b9d58aaa08
palaknarang05 <palaknarang05@gmail.com>
2026-02-06 01:48:03 +0530

    Add root URL view

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,6 +5,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Chemical Equipment API is running!"})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
